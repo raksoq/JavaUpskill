06f97e5da87c2b8724dfa7480ca98c125f0d3701
Oskar Kunicki <okunicki@objectivity.co.uk>
2021-09-17 22:58:15 +0200

    jacoco and param test example but not working

---
+++ src/test/java/PersonParamTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+public class PersonParamTest {
+
+    private Person person;
+
+    @TestFactory
+    Iterator<DynamicTest> dynamicTestFromCollection(){
+        return Arrays.asList(
+            dynamicTest("1st test",         () -> assertEquals(1, 1)),
+            dynamicTest("2nd dynamic test", () -> assertEquals(1, 1))
+        ).iterator();
+    }
+
+// Param test not working
+//    @ParameterizedTest
+//    @ValueSource(ints = {2, 5, 6, 18, 19})
+//    void checkIfYoung(int age){
+//        person =  new Person(age);
+//        assertEquals("You are young.",person.amIOld());
+//
+//    }
+}
